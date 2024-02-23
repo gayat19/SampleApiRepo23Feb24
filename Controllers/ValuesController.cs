6c2fb919aced2094b5641bf44a23e2c05cdf15d6
Gayathri Mahadevan <gayat_19@yahoo.com>
2024-02-23 12:52:10 +0530

    Values Controller added

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace SampleAPIApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        int num1;
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -15,6 +16,7 @@ namespace SampleAPIApp.Controllers
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
+            num1 = 100;
             _logger = logger;
         }
 
